0f9a851443f20e33030c3c5e323bdbf0be5d63f1
Nic Jackson <jackson.nic@gmail.com>
2016-08-06 20:03:15 +0100

    updated minke version added comments

--- servicediscovery/service-discovery.go
+++ servicediscovery/service-discovery.go
@@ -1,5 +1,6 @@
 package servicediscovery
 
+// Service represents a backend service which is registered with the dynamci service registry
 type Service struct {
 	Node           string
 	Address        string
@@ -10,6 +11,13 @@ type Service struct {
 	ServicePort    int
 }
 
+// Backend represents a dynamic service registry like Consul or etcd
 type Backend interface {
-	Services() []*Service
+	//Services returns a map of Service from the Backend
+	// The key to the map is the name of the service
+	// Services returns an array as there may be more than one endpoint registered for a service name
+	Services() map[string][]*Service
+
+	//ServicesByTag returns an array of Service for the given service name and tag
+	ServicesByTag(service string, tag string) []*Service
 }
